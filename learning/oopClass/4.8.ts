cfad1f00a4177285beb22d70aec4902eec7eb6e0
altaj1 <altaj1019@gmail.com>
2024-11-08 23:38:53 +0600

    polymorphisom

--- learning/oopClass/4.8.ts
+++ learning/oopClass/4.8.ts
@@ -28,5 +28,49 @@
     const student = new Student();
     const developer = new Developer();
 
-    getSleepingHours(developer)
+    // getSleepingHours(person)
+    // getSleepingHours(developer)
+    // getSleepingHours(student)
+
+    // ------------------------------------------
+
+    class Cshap {
+        getArea(): number{
+            return 0;
+        }
+    }
+
+    class Ccircle extends Cshap {
+        radius: number;
+        constructor (radius: number){
+            super();
+            this.radius = radius;
+        }
+        getArea():number {
+            return Math.PI * this.radius * this.radius;
+        }
+    }
+
+    class Creactangle extends Cshap {
+        height : number;
+        width: number;
+        constructor(height: number, width: number){
+            super();
+            this.height = height;
+            this.width = width;
+        }
+        getArea(): number {
+            return this.height * this.width;
+        }
+    } 
+
+
+    const getShapeArea = (param: Cshap): void=>{
+        console.log(param.getArea())
+    }
+    const shap = new Cshap();
+    const circle = new Ccircle(10);
+    const reactangle = new Creactangle(10, 20);
+
+    getShapeArea(circle)
 }
