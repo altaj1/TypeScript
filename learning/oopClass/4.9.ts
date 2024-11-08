cd35ff722bd82e3132d7db4edd7c44626baa3edf
altaj1 <altaj1019@gmail.com>
2024-11-09 01:11:29 +0600

    abstraction

---
+++ learning/oopClass/4.9.ts
@@ -0,0 +1,28 @@
+{
+    // ts-node-dev --respawn --transpile-only oopClass/4.9.ts
+    // abstraction  : 1. interface 2. abstract
+    interface Ivehicle1 {
+        startEngine(): void;
+        stopEngine():void;
+        move(): void;
+    }
+
+    class Ccar1 implements Ivehicle1 {
+        startEngine(): void {
+            console.log(`I am starting the car engine`);
+        }
+        stopEngine(): void {
+            console.log("I am stopping the car engine");
+        }
+
+        move(): void {
+            console.log(`I am moving the car`);
+          }
+          test() {
+            console.log(`I am just testing`);
+          }
+    }
+    const  toyotaCar = new Ccar1();
+    toyotaCar.startEngine();
+    
+}
